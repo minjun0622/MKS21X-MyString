a55e7f78b29d957fa4ab28f92dafcab63ade4413
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-11-21 12:59:23 -0500

    Worked it on a little more.

--- MyString.java
+++ MyString.java
@@ -1,6 +1,21 @@
 public class MyString implements CharSequence,Comparable<CharSequence>{
   private char[] data;
   public MyString(CharSequence s){
+    char data [] = char newdata [s.length()];
 
+  }
+//Returns the char value at the specified index. An index ranges from zero to length() - 1.
+//The first char value of the sequence is at index zero, the next at index one, and so on, as for array indexing.
+//If the char value specified by the index is a surrogate, the surrogate value is returned.
+  public char charAt(int index) {
+    if (index < 0 || index > data.length) {
+      throw new IndexOutofBoundsException("Index cannot be negative or greater/equal to the length");
+    }
+    return newdata [s.length()];
+    }
+
+    public int length(int index) {
+      return newdata [s.length()];
+    }
   }
 }
