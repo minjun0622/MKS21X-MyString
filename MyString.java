fead203be747603e12db1f226c581122178e5acf
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-11-22 15:14:30 -0500

    Added comments!

--- MyString.java
+++ MyString.java
@@ -1,8 +1,10 @@
-public class MyString implements CharSequence,Comparable<CharSequence>{
+public class MyString implements CharSequence, Comparable<CharSequence>{
   private char[] data;
   public MyString(CharSequence s){
     char data [] = char newdata [s.length()];
-
+    for (int i = 0; i < s.length(); i++) {
+      data[i] = s.charAt(i);
+    }
   }
 //Returns the char value at the specified index. An index ranges from zero to length() - 1.
 //The first char value of the sequence is at index zero, the next at index one, and so on, as for array indexing.
@@ -13,9 +15,22 @@ public class MyString implements CharSequence,Comparable<CharSequence>{
     }
     return newdata [s.length()];
     }
-
+//Returns the length of the sequence at the index specified.
     public int length(int index) {
       return newdata [s.length()];
     }
+//Charsequence is a sequence of strings,
+//subSequence returns the sequence of strings beginning from start integer and end intger.
+    public CharSequence subSequence(int start, int end) {
+      if (start < 0 || end < 0 || end > s.length() || start > end) {
+        throw new IndexOutofBoundsException("Start is not negative,
+        end is not negative, end should not be greater than length, start is not greater than end.");
+      }
+      return newdata[start, end]
+    }
+//Returns a string containing the characters in this sequence in the same order as this sequence.
+//The length of the string will be the length of this sequence.
+    public String toString () {
+      return newdata;
+    }
   }
-}
