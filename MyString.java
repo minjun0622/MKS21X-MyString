053c08d09d93c00de3969b5a168b1a3ae873b970
Minjun Seo <minjunseo@Minjuns-MacBook-Pro.local>
2018-11-26 11:56:13 -0500

    Added the main method. Thank you Timothy for providing the main.

--- MyString.java
+++ MyString.java
@@ -58,22 +58,100 @@ public class MyString implements CharSequence, Comparable<CharSequence>{
       }
       if (data.length != o.length()) {
         return data.length - o.length();
+        //checks if the length of data is not equal to length of charsequence o.
       }
       for (int i = 0; i < data.length; i++) {
         if (data[i] != o.charAt(i)) {
           return data[i] - o.charAt(i);
+          //uses a for loop to check if the specified value at data is not equal to charsequence o.
         }
       }
       return 0;
+      //if they're the same, then return 0.
     }
     //a negative integer, zero, or a positive integer as this object is less than,
     //equal to, or greater than the specified object.
 
     public static void main(String[] args) {
-      try {
-        System.out.println("")
-      }
 
+    // sole constructor test
+    MyString msg = new MyString("droid");
+    System.out.println("MyString msg = new MyString(\"charlotte\")");
 
-    }
+    // toString()
+    System.out.println("msg: " + msg);                                                  // "droid"
+
+    // length()
+    System.out.println("msg.length(): " + msg.length());                                // 5
+
+    System.out.println();
+
+    // charAt() exception catch
+    System.out.println("msg.charAt(-1):");
+    try {
+      System.out.println(msg.charAt(-1));                             // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    System.out.println("msg.charAt(15):");
+    try {
+      System.out.println(msg.charAt(15));                             // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // charAt()
+    System.out.println("msg.charAt(0): " + msg.charAt(0));                              // "d"
+    System.out.println("msg.charAt(3): " + msg.charAt(3));                              // "i"
+
+    System.out.println();
+
+    // subSequence() exception catch
+    // start < 0
+    System.out.println("msg.subSequence(-1,4):");
+    try {
+      System.out.println(msg.subSequence(-1,4));                      // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // end < 0
+    System.out.println("msg.subSequence(1,-4):");
+    try {
+      System.out.println(msg.subSequence(1,-4));                      // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // start > end
+    System.out.println("msg.subSequence(11,4):");
+    try {
+      System.out.println(msg.subSequence(11,4));                      // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // end > length()
+    System.out.println("msg.subSequence(1,14):");
+    try {
+      System.out.println(msg.subSequence(1,14));                      // IndexOutOfBoundsException
+    } catch(IndexOutOfBoundsException e) { e.printStackTrace(); }
+
+    System.out.println();
+
+    // subSequence()
+    System.out.println("msg.subSequence(0,3): " + msg.subSequence(0,3));                // "dro"
+    System.out.println("msg.subSequence(2,5): " + msg.subSequence(2,5));                // "oid"
+
+    System.out.println();
+
+    // compareTo()
+    System.out.println("msg.compareTo(\"droid\"): " + msg.compareTo("droid"));          // 0
+    System.out.println("msg.compareTo(\"bat\"): " + msg.compareTo("bat"));              // 1
+    System.out.println("msg.compareTo(\"seventh\"): " + msg.compareTo("seventh"));      // -1
+    System.out.println("msg.compareTo(\"drank\"): " + msg.compareTo("drank"));          // 1
+    System.out.println("msg.compareTo(\"drunk\"): " + msg.compareTo("drunk"));          // -1
+
+  }
   }
